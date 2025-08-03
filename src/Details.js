51662a70c93888d2ba7fbc874d53ad33ec6a015b
Rendkrisna19 <muhammadrendykrisna@gmail.com>
2025-08-03 11:01:58 +0700

    update

--- src/Details.js
+++ src/Details.js
@@ -24,12 +24,14 @@ import laravel from "./assets/techstack/php.png";
 import mysql from "./assets/techstack/mysql.png";
 import python from "./assets/techstack/python.jpg";
 // Porject Images
-import projectImage1 from "./assets/projects/project1.jpg";
-import projectImage2 from "./assets/projects/project2.jpg";
-import projectImage3 from "./assets/projects/project3.jpg";
-import projectImage4 from "./assets/projects/project4.jpg";
-import projectImage5 from "./assets/projects/project5.jpg";
-import projectImage6 from "./assets/projects/project6.jpg";
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.png";
+import projectImage6 from "./assets/projects/project6.png";
+import projectImage7 from "./assets/projects/project7.png";
+import projectImage8 from "./assets/projects/project8.png";
 
 // Logos
 export const logos = {
@@ -42,7 +44,7 @@ export const personalDetails = {
   name: "Rendy Krisna",
   tagline: "I am Fullstack Developer",
   img: profile,
-  about: `Saya percaya teknologi adalah seni. Sebagai seorang perajin digital, saya membangun pengalaman web yang tidak hanya bekerja dengan sempurna, tetapi juga terasa hidup dan intuitif. Dari merajut piksel di antarmuka (front-end) hingga merakit logika di balik layar (back-end), setiap baris kode adalah langkah untuk menciptakan sesuatu yang bermakna. Saya bersemangat dalam memecahkan masalah dan siap berkolaborasi untuk membangun produk digital yang luar biasa.`,
+  about: `Saya percaya teknologi adalah seni. Sebagai seorang Developer, saya membangun pengalaman web yang tidak hanya bekerja dengan sempurna, tetapi juga terasa hidup dan intuitif. Dari merajut piksel di antarmuka (front-end) hingga merakit logika di balik layar (back-end), setiap baris kode adalah langkah untuk menciptakan sesuatu yang bermakna. Saya bersemangat dalam memecahkan masalah dan siap berkolaborasi untuk membangun Sistem yang bermanfaat, yang dapat memecahkan masalah.`,
 };
 
 // Enter your Social Media URLs here
@@ -123,57 +125,67 @@ export const techStackDetails = {
 // Enter your Project Details here
 export const projectDetails = [
   {
-    title: "Project title 1",
+    title: "Wesco | Sistem Pengelolaan Tanki Delivery Pertamina",
     image: projectImage1,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
+    description: `Sistem Ini ialah sistem yang saya buat melalui pesanan client yang saya dapatkan dari seorang karyawan PT pertamina, mereka ingin mengembangkan sistem yang sebelumnya manual menjadi semi digitalisasi dengan membuat sistem agar pekerjaan lebih mudah dan efektif. Sistem ini saya buat selama lebih kurang 1 bulan pengerjaan.`,
+    techstack: "PHP, MYSQL, Tailwind CSS, JavaScript",
+    previewLink: "https://",
+    githubLink: "https://github.com/",
   },
   {
-    title: "Project title 2",
+    title: "Company Profile Solaria Indonesia",
     image: projectImage2,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
+    description: `Website Company Profile Solaria Indonesia adalah website yang saya buat untuk memenuhi kebutuhan perusahaan dalam memperkenalkan diri mereka kepada publik. Website ini dirancang dengan tampilan yang menarik dan responsif, sehingga dapat diakses dengan baik di berbagai perangkat. Saya menggunakan teknologi React Js dan Tailwind Css untuk membangun website ini.`,
+    techstack: "React Js, Tailwind CSS",
+    previewLink: "https://google.com",  
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 3",
+    title: "Sistem Inventaris Barang Kabupaten Kemang",
     image: projectImage3,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
+    description: `Website Sistem Inventaris Barang Kabupaten Kemang adalah sebuah sistem yang saya buat untuk membantu pemerintah daerah dalam mengelola inventaris barang. Sistem ini dirancang untuk memudahkan pencatatan, pengelolaan, dan pelaporan inventaris barang secara efisien. Saya menggunakan teknologi PHP dan MySQL untuk membangun sistem ini.`,
+    techstack: "HTML/CSS, JavaScript, PHP, MySQL",
+    previewLink: "https://kecamatankemangkabbogor.site/",
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 4",
+    title: "Sistem Informasi Bakoelide",
     image: projectImage4,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
+    description: `Website ini merupakan website yang saya buat waktu saya magang di PT DIGITAL KREATIF INDONESIA, sebuah perusahaan yang bergerak di bidang digital marketing. Website ini dirancang untuk memberikan informasi tentang layanan yang ditawarkan oleh PT agency, serta untuk mempromosikan produk dan layanan mereka kepada klien potensial. Saya menggunakan teknologi React Js dan Tailwind Css, Dan laravel untuk membangun website ini.`,
+    techstack: "Laravel, React Js, Tailwind CSS",
+    previewLink: "https://bakoelide.com/",
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 5",
+    title: "Sistem Informasi Desa Singgamanik",
     image: projectImage5,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
+    description: `Website Sistem Informasi Desa Singgamanik adalah sebuah sistem yang saya buat untuk membantu pemerintah desa dalam mengelola informasi dan layanan publik. Sistem ini dirancang untuk memudahkan warga desa dalam mengakses informasi terkait layanan publik, seperti administrasi kependudukan, kesehatan, pendidikan, dan lain-lain. Saya menggunakan teknologi PHP dan MySQL untuk membangun sistem ini.`,
+    techstack: "PHP, Mysql, HTML/CSS, JavaScript",
     previewLink: "https://google.com",
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 6",
+    title: "Website Latahzan Reborn",
     image: projectImage6,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
+    description: `Website Latahzan Reborn Adalah sebuah website yang saya buat untuk Team Basket Latahzan Reborn, sebuah tim basket yang berbasis di Medan. Website ini dirancang untuk memberikan informasi tentang tim, jadwal pertandingan, dan berita terkini seputar dunia basket. Saya menggunakan teknologi React Js & Tailwind Css untuk membangun website ini.`,
+    techstack: "React Js, Tailwind CSS",
+    previewLink: "https://latahzan-reborn.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Website Codifyhub",
+    image: projectImage7,
+    description: `Website Codifyhub adalah website usaha jasa pembuatan Website Saya sendiri, yang saya buat untuk mempromosikan jasa pembuatan website yang saya tawarkan. Website ini dirancang dengan tampilan yang menarik dan responsif, sehingga dapat diakses dengan baik di berbagai perangkat. Saya menggunakan teknologi React Js dan Tailwind Css untuk membangun website ini.`,
+    techstack: "React Js, Tailwind CSS",
+    previewLink: "https://codify-hub-one.vercel.app/",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Website Pringsewu Tani Digital",
+    image: projectImage8,
+    description: `Website Pringsewu Tani Digital adalah sebuah website yang saya buat untuk mempromosikan produk pertanian lokal di Kabupaten Pringsewu, Lampung. Website ini dirancang untuk memberikan informasi tentang produk pertanian, harga, dan cara pemesanan. Saya menggunakan teknologi React Js dan Tailwind Css untuk membangun website ini.`,
+    techstack: "React Js, Tailwind CSS",
+    previewLink: "https://pringsewu-tani-digital.vercel.app/",
     githubLink: "https://github.com",
   },
 ];
